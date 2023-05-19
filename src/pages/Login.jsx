57550698eb1fade4db0955ee78bdd6d6c61156ae
Alejosmurf03 <vegariverosmanuelalejandro@gmail.com>
2023-05-19 11:21:05 -0500

    Se realizan los estilos del login y registro de usuarios

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 const baseURL = "http://localhost:8080/login";
 import urlImg from "./../assets/img/logo.png";
+import urlImgLogo from "./../assets/img/img-login.jpg";
+import urlImgLogoLiti from "./../assets/img/icon-liti-login.png";
+
 const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -33,29 +36,87 @@ const Login = (props) => {
 
   return (
     <>
-      <header class="fixed-top d-flex align-items-center">
-        <div class="container d-flex align-items-center justify-content-lg-center">
+      <header
+        id="header"
+        className="fixed-top d-flex align-items-center bg-white"
+      >
+        <div className="container d-flex align-items-center justify-content-center">
           <a
             href="https://www.litigando.com/index.html"
-            class="me-auto lg:me-0"
+            className="logo me-auto me-lg-0"
           >
-            <img src={urlImg} alt="Logo_liti" width={300}></img>
+            <img
+              src={urlImg}
+              alt="Logo_liti"
+              width={300}
+              className="img-fluid"
+            ></img>
           </a>
         </div>
       </header>
-      <form onSubmit={handlerSubmit}>
-        <input
-          placeholder="Correo Electronico"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-        <input
-          placeholder="Contraseña"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-        <button>Ingresar</button>
-      </form>
+      <br/>
+      <section id="st-login" className="mt-5">
+        <div className="container h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col col-xl-10">
+              <div className="card rounded-5">
+                <div className="row g-0">
+                  <div className="col-md-6 col-lg-5 d-none d-md-block">
+                    <img
+                      src={urlImgLogo}
+                      alt="Img_login"
+                      className="img-fluid rounded-start-5"
+                    />
+                  </div>
+                  <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                    <div className="card-body p-4 p-lg-5 text-black">
+                      <form onSubmit={handlerSubmit}>
+                        <div className="d-flex align-items-center mb-3 pb-1">
+                          <img
+                            src={urlImgLogoLiti}
+                            alt="Liti_icon"
+                            className="fa-2x me-3"
+                          />
+                          <span className="h2 fw-bold mb-0">Bienvenidos</span>
+                        </div>      
+                        <br/>
+                        <div class="form-outline mb-2">
+                          <input
+                            className="form-control form-control-lg text-uppercase"
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                          />
+                          <label class="form-label">Usuario</label>
+                        </div>
+                        <div className="form-outline mb-2">
+                          <input
+                            className="form-control form-control-lg "
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                          />
+                          <label class="form-label">Contraseña</label>
+                        </div>
+                        <a href="#" class="mb-2 pb-lg-2 small text-muted">
+                          ¿Olvidaste la contraseña?
+                        </a>
+                        <br/>
+                        <a href="#" class="small text-muted">
+                          Política de privacidad
+                        </a>
+                        <div className="pt-1 mb-4">
+                          <button className="btn btn-warning btn-lg btm-block text-light">
+                            Ingresar
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
